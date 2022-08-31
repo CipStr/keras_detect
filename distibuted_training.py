f190ab0686de0443d14d48df576f035a3e82e4c8
CipStr <cipdev16@gmail.com>
2022-08-31 15:19:39 +0200

    First experiments on distributed training

---
+++ distibuted_training.py
@@ -0,0 +1,67 @@
+import json
+
+import tensorflow as tf
+from tensorflow import keras
+import os
+# set up TF_CONFIG environment variable for distributed training
+os.environ['TF_CONFIG'] = json.dumps({
+    'cluster': {
+        'worker': ["localhost:12345", "localhost:23456"]
+    },
+    'task': {'type': 'worker', 'index': 0}
+})
+
+def get_compiled_model():
+    # Make a simple 2-layer densely-connected neural network.
+    inputs = keras.Input(shape=(784,))
+    x = keras.layers.Dense(256, activation="relu")(inputs)
+    x = keras.layers.Dense(256, activation="relu")(x)
+    outputs = keras.layers.Dense(10)(x)
+    model = keras.Model(inputs, outputs)
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    )
+    return model
+
+
+def get_dataset():
+    batch_size = 32
+    num_val_samples = 10000
+
+    # Return the MNIST dataset in the form of a [`tf.data.Dataset`](https://www.tensorflow.org/api_docs/python/tf/data/Dataset).
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    # Preprocess the data (these are Numpy arrays)
+    x_train = x_train.reshape(-1, 784).astype("float32") / 255
+    x_test = x_test.reshape(-1, 784).astype("float32") / 255
+    y_train = y_train.astype("float32")
+    y_test = y_test.astype("float32")
+
+    # Reserve num_val_samples samples for validation
+    x_val = x_train[-num_val_samples:]
+    y_val = y_train[-num_val_samples:]
+    x_train = x_train[:-num_val_samples]
+    y_train = y_train[:-num_val_samples]
+    return (
+        tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(batch_size),
+        tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(batch_size),
+        tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size),
+    )
+
+# disable GPU
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+# Create a MirroredStrategy.
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+print("Number of devices: {}".format(strategy.num_replicas_in_sync))
+
+# Open a strategy scope.
+with strategy.scope():
+    # Everything that creates variables should be under the strategy scope.
+    # In general this is only model construction & `compile()`.
+    model = get_compiled_model()
+
+# Train the model on all available devices.
+train_dataset, val_dataset, test_dataset = get_dataset()
+model.fit(train_dataset, epochs=2, validation_data=val_dataset)
