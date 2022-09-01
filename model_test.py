0e860128c5afe39ceb05b643887eb5d229b5a6af
CipStr <cipdev16@gmail.com>
2022-09-01 17:23:13 +0200

    Added model_test.py for testing purposes on models produced by containers

---
+++ model_test.py
@@ -0,0 +1,22 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+
+# disable GPU
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+# take random image from PetImages/Cat/ folder
+img = tf.keras.preprocessing.image.load_img("PetImages/Cat/1.jpg", target_size=(180, 180))
+# convert to numpy array
+img_array = tf.keras.preprocessing.image.img_to_array(img)
+# add a dimension to the array
+img_array = tf.expand_dims(img_array, 0)
+# load model
+model = keras.models.load_model("saved_model")
+# predict
+predictions = model.predict(img_array)
+# print result
+score = predictions[0]
+print(
+    "This image is %.2f percent Cat and %.2f percent Dog."
+    % (100 * (1 - score), 100 * score)
+)
